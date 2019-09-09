370a29531436c42bfc7db5971e1b14746d18c077
nitinkundu <50735799+nitinkundu@users.noreply.github.com>
2019-09-09 19:39:41 +0530

    Update deletebulkbuild.groovy

--- vars/deletebulkbuild.groovy
+++ vars/deletebulkbuild.groovy
@@ -18,7 +18,7 @@ def call() {
 def jobName = "N_D_P_P"
 
 // The range of build numbers to delete.
-def buildRange = "32-34"
+def buildRange = "41-44"
 def j = jenkins.model.Jenkins.instance.getItem(jobName);
 def r = RangeSet.fromString(buildRange, true);
 j.getBuilds(r).each { it.delete() }
