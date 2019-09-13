c03245bb78605d0e1f6ab35a19708af015c23a64
vipuldubey0520 <49060553+vipuldubey0520@users.noreply.github.com>
2019-09-13 16:23:20 +0530

    Update deletebulkbuild.groovy

--- vars/deletebulkbuild.groovy
+++ vars/deletebulkbuild.groovy
@@ -52,7 +52,7 @@ Jenkins.instance.getItemByFullName(jobName).builds.findAll {
 
 //************************************************************************************************ Case 3
 // this housekeeping groovy script is to delete all failed build till some maximum build number.
-def jobName = "N_D_P_P"
+def jobName = "shared_lib_vipul"
 def maxNumber = 184
 Jenkins.instance.getItemByFullName(jobName).builds.findAll { it.result == Result.FAILURE && it.number <= maxNumber}.each { it.delete() }
 }
