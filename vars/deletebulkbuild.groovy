e20648ffbd3ce3077b5490f583c8366d30cc6614
vipuldubey0520 <49060553+vipuldubey0520@users.noreply.github.com>
2019-09-13 13:22:37 +0530

    Update deletebulkbuild.groovy

--- vars/deletebulkbuild.groovy
+++ vars/deletebulkbuild.groovy
@@ -13,8 +13,11 @@
 import jenkins.model.*;
 import hudson.model.Fingerprint.RangeSet;  
 def call() {
-  // The name of the job.
-/***def jobName = "Deploy"
+  
+//************************************************************************************************ Case 1
+  // give name of the job with the range of builds you want to delete .
+/***
+def jobName = "Deploy"
 
 // The range of build numbers to delete.
 def buildRange = "100-107"
@@ -22,7 +25,13 @@ def j = jenkins.model.Jenkins.instance.getItem(jobName);
 def r = RangeSet.fromString(buildRange, true);
 j.getBuilds(r).each { it.delete() }
 **/
- def jobName = "N_D_P_P"
+//************************************************************************************************
+  
+  
+//************************************************************************************************ Case 2
+//this will check for all builds and directly delete all builds below the max number mentioned
+/**
+def jobName = "N_D_P_P"
 def maxNumber = 20
 
 Jenkins.instance.getItemByFullName(jobName).builds.findAll {
@@ -30,4 +39,64 @@ Jenkins.instance.getItemByFullName(jobName).builds.findAll {
 }.each {
   it.delete()
 } 
+**/
+//************************************************************************************************
+  
+def deleteOldBuilds(item, Integer numberOfBuildsToKeep, Integer numberOfSuccessfulBuildsKept) {
+    def count = 1
+
+    println('Checking for Old Builds...')
+
+    for (build in item.getBuilds()) {
+        if(count++ >= numberOfBuildsToKeep) {
+            if(item.getBuildStatusIconClassName() == 'icon-blue' && numberOfSuccessfulBuildsKept == 0) {
+                println('Keep ' + build)
+            } else {
+                println('Deleting ' + build)
+                build.delete()
+            }
+        } else if(item.getBuildStatusIconClassName() == 'icon-blue') {
+            numberOfSuccessfulBuildsKept++
+        }
+    }
+    println('PRIOR BUILD COUNT: (' + count + ')')
+    println ''
+}
+
+def listJobObjects(item, Integer numberOfBuildsToKeep, Integer numberOfSuccessfulBuildsKept) {
+    if(item instanceof Project) {
+        println('PROJECT: (' + item.getName() + ')')
+        deleteOldBuilds(item, numberOfBuildsToKeep, numberOfSuccessfulBuildsKept)
+    } else if(item instanceof Folder) {
+        println ''
+        println('FOLDER: (' + item.getName() + ')')
+        println('*************************************')
+        for (subItem in item.items) {
+            listJobObjects(subItem, numberOfBuildsToKeep, numberOfSuccessfulBuildsKept)
+        }
+    } else if(item instanceof WorkflowMultiBranchProject) {
+        println('MULTIBRANCH-PROJECT: (' + item.getName() + ')')
+        for (subItem in item.items) {
+            listJobObjects(subItem, numberOfBuildsToKeep, numberOfSuccessfulBuildsKept)
+        }
+    }  else if(item instanceof WorkflowJob) {
+        println('MULTIBRANCH-JOB: (' + item.getName() + ')')
+        deleteOldBuilds(item, numberOfBuildsToKeep, numberOfSuccessfulBuildsKept)
+    } else if(item instanceof OrganizationFolder) {
+        println('ORG-FOLDER: (' + item.getName() + ')')
+        for (subItem in item.items) {
+            listJobObjects(subItem, numberOfBuildsToKeep, numberOfSuccessfulBuildsKept)
+        }
+    } else {
+        println('UNKNOWN: (' + item.getName() + ')')
+        println('CLASS: (' + item.getClass() + ')')
+        println('INSPECT: (' + item.inspect() + ')')
+    }
+}
+
+for (item in Jenkins.instance.items) {
+    println ''
+    listJobObjects(item, 10, 0)
+    println('*************************************')
+}
 }
