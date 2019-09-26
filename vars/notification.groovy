5f54b98b127aa42ad3c303039536fac0867f4ad4
vipuldubey0520 <49060553+vipuldubey0520@users.noreply.github.com>
2019-09-26 17:21:51 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,9 +1,6 @@
 //before using this below script please make sure you have slack configured and integrated to jenkin
 
 
-
-#!/usr/bin/env groovy
-
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
      slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
