bb8195ee77acbfbbd499962f1883a23215ff9156
vipuldubey0520 <49060553+vipuldubey0520@users.noreply.github.com>
2019-09-30 15:41:40 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -5,14 +5,14 @@ if ( buildResult == "SUCCESS" ) {
      slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
      emailext body: 'Build Success', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'vipuldevops1994@gmail.com'}
  }
-else if ( buildResult == "FAILURE" ) { 
+if ( buildResult == "FAILURE" ) { 
    slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
    emailext body: 'Build Failure', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'vipuldevops1994@gmail.com'
 }
-  else if ( buildResult == "UNSTABLE" ) { 
+if ( buildResult == "UNSTABLE" ) { 
     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
   }
-  else {
+if {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
   }
 }
