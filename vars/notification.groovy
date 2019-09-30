1b990f3af4852749c4a11bc0d77c52febc333dcd
vipuldubey0520 <49060553+vipuldubey0520@users.noreply.github.com>
2019-09-30 15:23:29 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -10,6 +10,7 @@ if ( buildResult == "SUCCESS" ) {
    slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
    emailext body: 'Build Failure', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'vipuldevops1994@gmail.com'
 }
+}
 
 
 
