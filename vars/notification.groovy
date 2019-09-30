ae14c039822e8585eb0272df40b8c507577094a2
vipuldubey0520 <49060553+vipuldubey0520@users.noreply.github.com>
2019-09-30 15:21:28 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -2,11 +2,15 @@
 
 
 def call(String buildResult) {
-if ( buildResult == "FAILURE" ) { 
-   slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
-   emailext body: 'Build Failure', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'vipuldevops1994@gmail.com'
- }
-  else ( buildResult == "SUCCESS" ) {
+if ( buildResult == "SUCCESS" ) {
      slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
      emailext body: 'Build Success', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'vipuldevops1994@gmail.com'}
+ }
+  else ( buildResult == "FAILURE" ) { 
+   slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+   emailext body: 'Build Failure', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'vipuldevops1994@gmail.com'
 }
+
+
+
+
